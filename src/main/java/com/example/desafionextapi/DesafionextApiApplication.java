dc8ce033122c6b4e6fbf3904e950dfe64eaf80f0
Thiago Souza <83614983+thgsdsouza@users.noreply.github.com>
2021-10-19 12:13:08 -0300

    Projeto inicial (#2)

---
+++ src/main/java/com/example/desafionextapi/DesafionextApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafionextapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafionextApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafionextApiApplication.class, args);
+	}
+
+}
