b12250af77330a3e98a5e2f4cb3a1c89bb3ae108
Thiago Souza <83614983+thgsdsouza@users.noreply.github.com>
2021-10-19 10:44:03 -0300

    Projeto inicial

---
+++ src/test/java/com/example/desafionextapi/DesafionextApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafionextapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafionextApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
