67d673dfe6b6333f5e5414f7d73444db533a0431
daniellylima <daniinovaes@gmail.com>
2021-10-20 08:58:47 -0300

    correçao do entities>Project.

--- src/main/java/com/example/desafionextapi/entities/Projeto.java
+++ src/main/java/com/example/desafionextapi/entities/Projeto.java
@@ -2,17 +2,11 @@ package com.example.desafionextapi.entities;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
 public class Projeto {
-    @Id
     private long id;
 
     public Projeto (long id) {
-        if (id < 1) {
-            System.out.println("O número de identificação do projeto não pode ser menor que 1");
-        }else {
-            this.setId(id);
-        }
+        this.setId(id);
     }
 
     private void setId(long id) {
