f176594f2e1269be660d6b6e1173fa5551a9af40
Denis Ferreira <denisferreira93@hotmail.com>
2021-10-20 15:38:49 -0300

    Implementação do endpoint que exibe todos os projetos cadastrados

---
+++ src/main/java/com/example/desafionextapi/controller/ProjetoController.java
@@ -0,0 +1,22 @@
+package com.example.desafionextapi.controller;
+
+import com.example.desafionextapi.entities.Projeto;
+import com.example.desafionextapi.services.ProjetoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProjetoController {
+
+    @Autowired
+    private ProjetoService projetoService;
+
+    @GetMapping("/projects")
+    public ResponseEntity<List<Projeto>> allProjects(){
+        return ResponseEntity.ok().body(projetoService.getProjetos());
+    }
+}
