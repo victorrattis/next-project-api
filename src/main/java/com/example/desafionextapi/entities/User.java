f5bcec6ded0bbb6a14c711d8b43e70a4215fb0c5
Lucca Brito <luccabrito@pop-os.localdomain>
2021-10-20 20:36:54 -0300

    refatoração do código dos controllers do User e do Project

---
+++ src/main/java/com/example/desafionextapi/entities/User.java
@@ -0,0 +1,13 @@
+package com.example.desafionextapi.entities;
+
+public class User {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+}
---
+++ src/main/java/com/example/desafionextapi/services/ProjectService.java
@@ -0,0 +1,16 @@
+package com.example.desafionextapi.services;
+import com.example.desafionextapi.entities.Project;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProjectService {
+
+    public List<Project> projetos = new ArrayList<>();
+
+    public void saveProject(Project project){
+        projetos.add(project);
+    }
+}
