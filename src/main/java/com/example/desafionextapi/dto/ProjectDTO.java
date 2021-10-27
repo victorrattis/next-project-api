c6c1ec09ce650633b1d7073f992df3e7d63b33e9
Denis Ferreira <denisferreira93@hotmail.com>
2021-10-27 12:12:13 -0300

    Implementando a classe ProjectDTO no pacote DTO

---
+++ src/main/java/com/example/desafionextapi/dto/ProjectDTO.java
@@ -0,0 +1,14 @@
+package com.example.desafionextapi.dto;
+
+public class ProjectDTO {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
