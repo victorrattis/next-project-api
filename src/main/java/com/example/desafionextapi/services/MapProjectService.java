80aaa9c1a58d032efc9f578add5de295161ab076
Denis Ferreira <denisferreira93@hotmail.com>
2021-10-27 12:14:08 -0300

    Implementado a classe MapProjectService para utilizar o padrão DTO

---
+++ src/main/java/com/example/desafionextapi/services/MapProjectService.java
@@ -0,0 +1,30 @@
+package com.example.desafionextapi.services;
+
+import com.example.desafionextapi.dao.ProjectDAO;
+import com.example.desafionextapi.dto.ProjectDTO;
+import com.example.desafionextapi.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapProjectService {
+
+    @Autowired
+    private ProjectDAO projectDAO;
+
+    public List<ProjectDTO> getAllUsersLocation() {
+        return ((List<Project>) projectDAO
+                .findAll())
+                .stream()
+                .map(this::convertToUserLocationDTO)
+                .collect(Collectors.toList());
+    }
+
+    private ProjectDTO convertToUserLocationDTO(Project project) {
+        ProjectDTO projectDTO = new ProjectDTO();
+        projectDTO.setId(project.getId());
+        return projectDTO;
+    }
+
+}
